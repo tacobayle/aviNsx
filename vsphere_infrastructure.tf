e44d36a852d4f2d82adbfb602a3d1786deed754e
tacobayle <nicolas.bayle@gmail.com>
2021-01-12 09:56:55 +0000

    20210112

--- vsphere_infrastructure.tf
+++ vsphere_infrastructure.tf
@@ -1,34 +1,33 @@
-#
 data "vsphere_datacenter" "dc" {
   name = var.vcenter.dc
 }
-#
+
 data "vsphere_compute_cluster" "compute_cluster" {
   name          = var.vcenter.cluster
   datacenter_id = data.vsphere_datacenter.dc.id
 }
-#
+
 data "vsphere_datastore" "datastore" {
   name = var.vcenter.datastore
   datacenter_id = data.vsphere_datacenter.dc.id
 }
-#
+
 data "vsphere_resource_pool" "pool" {
   name          = var.vcenter.resource_pool
   datacenter_id = data.vsphere_datacenter.dc.id
 }
-#
+
 data "vsphere_network" "networkMgt" {
   name = var.networkMgt
   datacenter_id = data.vsphere_datacenter.dc.id
 }
-#
+
 data "vsphere_network" "networkBackend" {
   depends_on = [time_sleep.wait_segment]
   name = var.backend["network"]
   datacenter_id = data.vsphere_datacenter.dc.id
 }
-#
+
 data "vsphere_network" "networkClient" {
   depends_on = [time_sleep.wait_segment]
   name = var.client["network"]
@@ -53,7 +52,6 @@ resource "vsphere_folder" "folderSe" {
   datacenter_id = data.vsphere_datacenter.dc.id
 }
 
-#
 resource "vsphere_tag_category" "ansible_group_backend" {
   name = "ansible_group_backend"
   cardinality = "SINGLE"
@@ -61,7 +59,7 @@ resource "vsphere_tag_category" "ansible_group_backend" {
     "VirtualMachine",
   ]
 }
-#
+
 resource "vsphere_tag_category" "ansible_group_client" {
   name = "ansible_group_client"
   cardinality = "SINGLE"
@@ -69,7 +67,7 @@ resource "vsphere_tag_category" "ansible_group_client" {
     "VirtualMachine",
   ]
 }
-#
+
 resource "vsphere_tag_category" "ansible_group_controller" {
   name = "ansible_group_controller"
   cardinality = "SINGLE"
@@ -77,7 +75,7 @@ resource "vsphere_tag_category" "ansible_group_controller" {
     "VirtualMachine",
   ]
 }
-#
+
 resource "vsphere_tag_category" "ansible_group_jump" {
   name = "ansible_group_jump"
   cardinality = "SINGLE"
